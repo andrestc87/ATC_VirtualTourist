a5a0c73be87b9228437d7e275bcfa41f616e3868
andrestc87 <andrestc87@gmail.com>
2020-08-23 20:57:45 -0500

    Initial Commit

---
+++ ATC_VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ATC_VirtualTourist
+//
+//  Created by andres tello campos on 8/23/20.
+//  Copyright © 2020 andres tello campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
